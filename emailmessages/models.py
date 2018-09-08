69edd99df3dcbcd637bb887f77f367baa5e6adbf
Oscar GG <profesor.oscar.gomez@gmail.com>
2018-09-09 00:09:38 +0200

    ModelForm

--- emailmessages/models.py
+++ emailmessages/models.py
@@ -7,10 +7,11 @@ from django.db import models
 class Provider(models.Model):
     smtp_server_address     =       models.CharField(max_length=128)
     tcp_port                =       models.IntegerField(default=995)
+    
 class Sender(models.Model):
     user                    =       models.CharField(max_length=128)
     password                =       models.CharField(max_length=128)
-    provider                =       models.ForeignKey(Provider)
+    provider                =       models.ForeignKey(Provider, on_delete=models.CASCADE)
 
 
 class Reminder(models.Model):
@@ -18,20 +19,24 @@ class Reminder(models.Model):
     text                    =       models.TextField(max_length=32768)
     final_date              =       models.DateField()
     
+class EMailAccount(models.Model):
+    email                   =       models.EmailField()
+    description             =       models.CharField(max_length=200)
     
 class EmailMessage(models.Model):
     
     #Signals when the mail sender must send this message. When
-    #this date arrive the mailer is allowed to send this email. The
+    #this date arrives the mailer is allowed to send this email. The
     #date is not exact!! 
     date           =       models.DateTimeField()
     
     
     
     #Who is going to receive this message?
-    to                      =       models.CharField(max_length=128)
+    to                      =       models.ForeignKey( EMailAccount, on_delete=models.CASCADE)
     subject                 =       models.CharField(max_length=256)
-    reminder                =       models.ForeignKey ( Reminder )
+    reminder                =       models.ForeignKey ( Reminder ,  on_delete=models.CASCADE)
+    sender                  =       models.ForeignKey ( Sender, on_delete=models.CASCADE)
     
     
     
--- emailmessages/views.py
+++ emailmessages/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-
+from django.forms import ModelForm
+from .models import EmailMessage
 # Create your views here.
 
 
+
+class EmailForm(ModelForm):
+    class Meta:
+        model = EmailMessage
+        fields=["date", "to", "subject", "reminder", "sender"]
+        
 def index(peticion):
     return render (peticion, 'emailmessages/index.html', None)
 
 def login(peticion):
     return render (peticion, 'emailmessages/login.html', None)
+
+def create_reminder(peticion):
+    if peticion.POST:
+        process_petition (peticion)
+    else:
+        formulario_email = EmailForm()
+        diccionario=dict()
+        diccionario["formulario_email"]=formulario_email
+        return render (peticion, 'emailmessages/create_reminder.html', diccionario)
